5689caf5aac4599bc7b8cce688205522743a6884
William Millen <wvmillen@gmail.com>
2025-11-10 18:09:37 -0500

    small fixes

--- apps/api/src/main/java/com/looped/shared/RedisConfig.java
+++ apps/api/src/main/java/com/looped/shared/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 import java.net.URI;
@@ -15,15 +16,28 @@ public class RedisConfig {
         if (redisUrl == null || redisUrl.isBlank()) {
             return new LettuceConnectionFactory();
         }
+
         URI uri = URI.create(redisUrl);
         RedisStandaloneConfiguration conf = new RedisStandaloneConfiguration();
         conf.setHostName(uri.getHost());
         conf.setPort(uri.getPort() == -1 ? 6379 : uri.getPort());
         if (uri.getUserInfo() != null && uri.getUserInfo().contains(":")) {
-            String password = uri.getUserInfo().substring(uri.getUserInfo().indexOf(':') + 1);
-            conf.setPassword(password);
+            String userInfo = uri.getUserInfo();
+            // Support optional "username:password" or just ":password"
+            int idx = userInfo.indexOf(':');
+            String password = idx >= 0 ? userInfo.substring(idx + 1) : userInfo;
+            if (!password.isBlank()) {
+                conf.setPassword(password);
+            }
+        }
+
+        boolean useSsl = uri.getScheme() != null && uri.getScheme().equalsIgnoreCase("rediss");
+        if (useSsl) {
+            LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                    .useSsl()
+                    .build();
+            return new LettuceConnectionFactory(conf, clientConfig);
         }
         return new LettuceConnectionFactory(conf);
     }
 }
-
